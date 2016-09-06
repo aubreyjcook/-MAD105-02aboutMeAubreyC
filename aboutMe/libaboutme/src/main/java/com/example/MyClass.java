37cbb395dc2519942f4134ac4ce5c47054ffd2af
aubreyjcook <aubreyjcook@gmail.com>
2016-09-06 13:16:28 -0500

    complete

---
+++ aboutMe/libaboutme/src/main/java/com/example/MyClass.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String myName = "Aubrey Cook";
+        String major = "General Education";
+        double gpa = 3.4;
+        char grade = 'A';
+        int started = 1989;
+        System.out.println("Hello, my name is " + myName);
+        System.out.println("I am majoring in " + major);
+        System.out.println("My goal this semester is to get a " + gpa +
+                " or higher for my GPA");
+        System.out.println("The grade I hope to get in this class is an " + grade);
+        System.out.println("I first started at MCC in " + started);
+    }
+}
